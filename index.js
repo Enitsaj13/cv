0ce4d94261627ebd814dce5d3b4f37b572721cde
Enitsaj13 <62202119+Enitsaj13@users.noreply.github.com>
2021-07-31 00:50:57 +0800

    Add initial cv website files

---
+++ index.js
@@ -0,0 +1,12 @@
+//document.getElementById("count-el").innerText = 5
+
+// let firstBatch = 5
+// let seconBatch = 7
+// let count = firstBatch + seconBatch
+
+// console.log(count)
+
+let myAge = 35
+let humanDogRatio = 7
+
+let myDogAge = myAge * humanDogRatio
